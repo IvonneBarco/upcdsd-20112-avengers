01f248131e8726b1c83c3d73ba92146cb5bf9d2a
miguelmac20@gmail.com <miguelmac20@gmail.com@b89ab4e8-0a83-2e70-31c1-5fdb0900255f>
2011-09-18 20:21:30 +0000

    Actualizacion Login

--- trunk/AppFarmacia/src/pe/edu/upc/dsd/appfarmacia/AppFarmaciaActivity.java
+++ trunk/AppFarmacia/src/pe/edu/upc/dsd/appfarmacia/AppFarmaciaActivity.java
@@ -11,30 +11,51 @@ import android.widget.EditText;
 public class AppFarmaciaActivity extends Activity {
     /** Called when the activity is first created. */
 	
+	private EditText txtUserName;
+	private EditText txtPassword;
+	private Button btnAceptar;
+	private Button btnCancelar;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    
-        final EditText txtnombre=(EditText)findViewById(R.id.txtNombre);
-    	final Button btnentrar=(Button)findViewById(R.id.btnEntrar);
-    	
-        btnentrar.setOnClickListener(new OnClickListener(){
-        	//@Override
-            public void onClick(View v) {
-                //Creamos el Intent
-            	Intent intent = new Intent(AppFarmaciaActivity.this, FrmPantalla2.class);
-            	
-            	//Creamos la información a pasar entre actividades
-            	Bundle b = new Bundle(); 
-            	b.putString("NOMBRE", txtnombre.getText().toString());
-            	
-            	//Añadimos la información al intent
-            	intent.putExtras(b);
-            	
-            	//Iniciamos la nueva actividad
-                startActivity(intent);
-            }
+        
+        txtUserName=(EditText)findViewById(R.id.txtUserName);
+        txtPassword=(EditText)findViewById(R.id.txtPassword);
+        btnAceptar=(Button)findViewById(R.id.btnAceptar);
+        btnCancelar=(Button)findViewById(R.id.btnCancelar);
+        
+        btnAceptar.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				String username=txtUserName.getText().toString();
+				String password=txtPassword.getText().toString();
+				if (username.equalsIgnoreCase("admin")&& password.equals("admin12")){
+					//Creamos el Intent
+	            	Intent intent = new Intent(AppFarmaciaActivity.this, FrmPantalla2.class);
+	            	
+	            	//Creamos la información a pasar entre actividades
+	            	Bundle b = new Bundle(); 
+	            	b.putString("NOMBRE", username);
+	            	
+	            	//Añadimos la información al intent
+	            	intent.putExtras(b);
+	  
+	            	//Iniciamos la nueva actividad
+	                startActivity(intent);
+				}	
+			}
         });
+        btnCancelar.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
     }
 }
