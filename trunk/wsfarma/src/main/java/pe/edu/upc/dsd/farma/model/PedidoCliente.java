801cbd09cb825511165152ba2c08e8a30fe145d5
andulink@gmail.com <andulink@gmail.com@b89ab4e8-0a83-2e70-31c1-5fdb0900255f>
2011-09-18 19:29:40 +0000

    Creacion WSCliente

---
+++ trunk/wsfarma/src/main/java/pe/edu/upc/dsd/farma/model/PedidoCliente.java
@@ -0,0 +1,23 @@
+package pe.edu.upc.dsd.farma.model;
+
+public class PedidoCliente {
+	private int numero;
+	private String estado;
+	
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+}
