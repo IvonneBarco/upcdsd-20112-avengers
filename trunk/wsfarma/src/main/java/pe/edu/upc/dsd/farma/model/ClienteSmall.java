05fb7aeffcd0fb36aa252b6d6f01cf07d2e799e6
andulink@gmail.com <andulink@gmail.com@b89ab4e8-0a83-2e70-31c1-5fdb0900255f>
2011-09-28 06:15:34 +0000

    Creacion Rest Service - Registrar Cliente

---
+++ trunk/wsfarma/src/main/java/pe/edu/upc/dsd/farma/model/ClienteSmall.java
@@ -0,0 +1,25 @@
+package pe.edu.upc.dsd.farma.model;
+
+public class ClienteSmall {
+	private String DNI; // Identificador del cliente
+	private String Nombre; // Nombre del cliente
+	
+
+	public String getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(String DNI) {
+		this.DNI = DNI;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String Nombre) {
+		this.Nombre = Nombre;
+	}
+
+	
+}
