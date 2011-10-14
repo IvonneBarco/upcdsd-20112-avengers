5a00495bfedf62c1f01783cb179c85721fc6ff59
miguelmac20 <miguelmac20@gmail.com>
2011-10-14 01:26:26 +0000

    servicio para android

---
+++ wsfarma/src/main/java/pe/edu/upc/dsd/farma/service/rest/IAlmacen.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.dsd.farma.service.rest;
+
+public interface IAlmacen {
+	
+	public String listarPedidos();
+	public String listaDetallepedido(String numeroPedido);
+	public String validaUser();
+
+}
