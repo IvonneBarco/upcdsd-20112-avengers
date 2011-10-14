bb35ff255e29ea1aa2e3aba374d8b88fe2c79e0c
miguelmac20 <miguelmac20@gmail.com>
2011-10-14 01:25:46 +0000

    servicio para android

---
+++ wsfarma/src/main/java/pe/edu/upc/dsd/farma/model/DetallePedidoAlmacen.java
@@ -0,0 +1,68 @@
+package pe.edu.upc.dsd.farma.model;
+
+public class DetallePedidoAlmacen {
+	private int numeroPedido;
+	private int itemPedido;
+	private String codigoProducto;
+	private String descripcion;
+	private int cantidad;
+	private double importe;
+	private double total;
+
+	public int getNumeroPedido() {
+		return numeroPedido;
+	}
+
+	public void setNumeroPedido(int numeroPedido) {
+		this.numeroPedido = numeroPedido;
+	}
+
+	public int getItemPedido() {
+		return itemPedido;
+	}
+
+	public void setItemPedido(int itemPedido) {
+		this.itemPedido = itemPedido;
+	}
+
+	public String getCodigoProducto() {
+		return codigoProducto;
+	}
+
+	public void setCodigoProducto(String codigoProducto) {
+		this.codigoProducto = codigoProducto;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public double getImporte() {
+		return importe;
+	}
+
+	public void setImporte(double importe) {
+		this.importe = importe;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+}
