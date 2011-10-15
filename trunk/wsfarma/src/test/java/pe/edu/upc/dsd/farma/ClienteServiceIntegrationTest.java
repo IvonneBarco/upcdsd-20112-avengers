d3504720faf1373c10ade16c0abb2afa53f23552
andulink@gmail.com <andulink@gmail.com@b89ab4e8-0a83-2e70-31c1-5fdb0900255f>
2011-10-15 07:30:18 +0000

    Test

--- trunk/wsfarma/src/test/java/pe/edu/upc/dsd/farma/ClienteServiceIntegrationTest.java
+++ trunk/wsfarma/src/test/java/pe/edu/upc/dsd/farma/ClienteServiceIntegrationTest.java
@@ -1,15 +1,25 @@
 package pe.edu.upc.dsd.farma;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.HttpHeaders;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
+import pe.edu.upc.dsd.farma.model.Cliente;
 import pe.edu.upc.dsd.farma.model.ResponseSecuencial;
 
 import com.google.gson.Gson;
@@ -22,20 +32,48 @@ public class ClienteServiceIntegrationTest {
 	 */
 	private String URLCliente = "http://localhost:8080/wsfarma/rest/clienteRest/";
 	private Gson gson = new Gson();
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
 	@Test
 	public void testObtieneSecuencial() {
-		
+
 		String fullURL = URLCliente + "obtieneNumero";
-		ResponseEntity<String> response = restTemplate.getForEntity(fullURL, String.class);
-		
-		Integer secuencial = gson.fromJson(response.getBody(),ResponseSecuencial.class).getNumero();
-		
+		ResponseEntity<String> response = restTemplate.getForEntity(fullURL,
+				String.class);
+
+		Integer secuencial = gson.fromJson(response.getBody(),
+				ResponseSecuencial.class).getNumero();
+
 		Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
 		Assert.assertEquals(Integer.class, secuencial.getClass());
 	}
-	
+
+	@Test
+	public void testRegistrarCliente() {
+		String fullURL = URLCliente + "registrar";
+
+		Cliente cliente = new Cliente();
+		cliente.setDni("44992694");
+		cliente.setDireccion("Av. La Prueba");
+		cliente.setNombre("Daniel");
+		cliente.setTelefono("5369504");
+		cliente.setDistrito("San Miguel");
+		cliente.setEmail("a@a.com");
+		cliente.setPassword("abc123");
+		cliente.setFlagNotif("Si");
+
+		String jsonCliente = gson.toJson(cliente);
+
+		org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<String> entity = new HttpEntity<String>(jsonCliente,headers);
+		
+		
+		ResponseEntity<String> response = restTemplate.postForEntity(fullURL, entity, String.class);
+		Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+
 }
